e3b26fbed70bb5f655f758e3a58895b6a7ac2aec
krishiv potluri <149548741+kishpish@users.noreply.github.com>
2024-12-27 14:23:50 -0600

    Create pymol_plddt_visual.py

---
+++ pymol_plddt_visual.py
@@ -0,0 +1,31 @@
+# Load extension into pymol. In the pymol command prompt:
+# run https://raw.githubusercontent.com/cbalbin-bio/pymol-color-alphafold/master/coloraf.py
+
+
+from pymol import cmd
+
+
+def coloraf(selection="all"):
+
+    """
+
+    DESCRIPTION
+    Colors Alphafold structures by pLDDT
+
+    USAGE
+    coloraf sele
+
+    PARAMETERS
+
+    sele (string)
+    The name of the selection/object to color by pLDDT. Default: all
+    """
+
+    cmd.color("blue", f"({selection}) and b > 90")
+    cmd.color("cyan", f"({selection}) and b < 90 and b > 70")
+    cmd.color("yellow", f"({selection}) and b < 70 and b > 50")
+    cmd.color("orange", f"({selection}) and b < 50")
+
+
+cmd.extend("coloraf", coloraf)
+cmd.auto_arg[0]["coloraf"] = [cmd.object_sc, "object", ""]
